53e286af644ee4122e28a524860516d559b8d4a2
HPHuerta <hhernanp@gmail.com>
2024-03-12 23:57:38 -0400

    Add libsql and database token. Update Turso DB set up

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 
 import { env } from "~/env";
 
@@ -6,11 +8,17 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+const initializePrisma = (): PrismaClient => {
+  const libsql = createClient({
+    url: env.DATABASE_URL,
+    authToken: env.DATABASE_TOKEN,
   });
+
+  const adapter = new PrismaLibSQL(libsql);
+  return new PrismaClient({adapter});
+}
+
+export const db = globalForPrisma.prisma ?? initializePrisma();
+  
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
