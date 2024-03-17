cec5897085ff0b7ed723a1f01fca1670aef25a75
HPHuerta <hhernanp@gmail.com>
2024-03-16 21:58:57 -0400

    Refactor Card component and add Modal component

--- src/app/_components/Card/Card.tsx
+++ src/app/_components/Card/Card.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import { Fragment, useState } from "react";
-import { Dialog, Transition } from '@headlessui/react'
+import { useState } from "react";
 import CopySvg from "~/utils/CopySvg";
 import DeleteSvg from "~/utils/DeleteSvg";
 import EditSvg from "~/utils/EditSvg";
+import Modal from "~/ui/Modal";
 
 interface LinkProps {
   slug: string;
@@ -16,7 +16,7 @@ interface LinkProps {
 export default function Card({ slug, description, id }: LinkProps) {
   const [copyTooltip, setCopyTooltip] = useState(false);
 
-
+  
   const copyToClipboard = async () => {
     try {
       setCopyTooltip(true);
@@ -29,7 +29,7 @@ export default function Card({ slug, description, id }: LinkProps) {
     } finally {
       setTimeout(() => {
         setCopyTooltip(false);
-      }, 900);
+      }, 1200);
     }
   };
 
@@ -44,44 +44,11 @@ export default function Card({ slug, description, id }: LinkProps) {
         <EditSvg className="hover:fill-dark-violet" />
         <DeleteSvg className="hover:fill-dark-violet" />
       </div>
-        <Transition appear show={copyTooltip} as={Fragment}>
-          <Dialog as="div" className="relative z-10" onClose={setCopyTooltip}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black/25" />
-          </Transition.Child>
-          
-            <div className="fixed inset-0 ">
-              <div className="flex min-h-full items-center justify-center p-4 text-center">
-                <Transition.Child
-                  as={Fragment}
-                  enter="ease-out duration-300"
-                  enterFrom="opacity-0 scale-95"
-                  enterTo="opacity-100 scale-100"
-                  leave="ease-in duration-200"
-                  leaveFrom="opacity-100 scale-100"
-                  leaveTo="opacity-0 scale-95"
-                >
-                  <Dialog.Panel className="border border-white/10 max-w-md transform overflow-hidden rounded-2xl bg-dark-midnight p-6 text-left align-middle shadow-xl transition-all">
-                    <Dialog.Title
-                      as="h3"
-                      className="text-lg font-medium leading-6 text-white text-center"
-                    >
-                      Copied!
-                    </Dialog.Title>
-                  </Dialog.Panel>
-                </Transition.Child>
-              </div>
-            </div>
-          </Dialog>
-        </Transition>
+      <Modal 
+        title="Link Copied!" 
+        copyTooltip={copyTooltip} 
+        setCopyTooltip={setCopyTooltip}
+      />
     </section>
   )
 }
---
+++ src/ui/Modal.tsx
@@ -0,0 +1,54 @@
+import { Fragment, type ReactNode } from "react"
+import { Dialog, Transition } from '@headlessui/react'
+
+interface ModalProps {
+  title: string;
+  copyTooltip: boolean;
+  children?: ReactNode;
+  setCopyTooltip: (value: boolean) => void;
+}
+
+export default function Modal(props: ModalProps) {
+
+  return (
+    <Transition appear show={props.copyTooltip} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={props.setCopyTooltip}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 ">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="border border-white/10 max-w-md transform overflow-hidden rounded-2xl bg-dark-midnight p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-white text-center"
+                >
+                  {props?.title}
+                </Dialog.Title>
+                {props.children}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  )
+}
