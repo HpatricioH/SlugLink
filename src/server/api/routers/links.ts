358432c44c99c482d7793b6d1e6a6919e24760fe
HPHuerta <hhernanp@gmail.com>
2024-03-16 21:59:27 -0400

    Add link update and delete functionality

--- src/server/api/routers/links.ts
+++ src/server/api/routers/links.ts
@@ -12,23 +12,37 @@ const CreateLinksSchema = z.object({
   description: z.string(),
 })
 
+const UpdateLinkSchema = z.object({
+  id: z.number(),
+  url: z.string(),
+  slug: z.string(),
+  description: z.string()
+})
+
+const SingleLinkSchema = z.object({
+  id: z.number(),
+  url: z.string(),
+  slug: z.string(),
+  description: z.string()
+})
+
 export const linksRouter = createTRPCRouter({
-  hello: publicProcedure 
+  hello: publicProcedure
     .input(z.object({ text: z.string() }))
-    .query(({ input}) => {
+    .query(({ input }) => {
       return {
         greeting: `${input.text}`,
       };
     }),
 
   // Create Link 
-  create: protectedProcedure 
-    .input( CreateLinksSchema )
+  create: protectedProcedure
+    .input(CreateLinksSchema)
     .mutation(async ({ ctx, input }) => {
       return ctx.db.link.create({
         data: {
           ...input,
-          createdBy: { connect: {id: ctx.session.user.id }}
+          createdBy: { connect: { id: ctx.session.user.id } }
         }
       })
     }),
@@ -40,5 +54,27 @@ export const linksRouter = createTRPCRouter({
         createdBy: { id: ctx.session.user.id }
       }
     })
-  })
+  }),
+
+  // Edit Links 
+  updateLink: protectedProcedure
+    .input(UpdateLinkSchema)
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.link.update({
+        where: { id: input.id },
+        data: {
+          ...input,
+          creatorId: ctx.session.user.id
+        }
+      })
+    }),
+
+  // Delete Links
+  deleteLink: protectedProcedure
+    .input(SingleLinkSchema)
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.link.delete({
+        where: { id: input.id }
+      })
+    }),
 })
