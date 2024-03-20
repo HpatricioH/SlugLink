72d1c46eb74eb623f5b8b3572bc35be1069f0fe4
HPHuerta <hhernanp@gmail.com>
2024-03-19 23:50:37 -0400

    Refactor CardContainer and add TODO comment in page.tsx

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -4,17 +4,17 @@ import CreateLink from "../_components/CreateLink/CreateLink";
 
 export default async function Page() {
   const session = await getServerAuthSession();
-  
+
   if (!session) {
     redirect('/')
   }
-
+  // TODO: sort by date created to show the first link created. 
   return (
     <section className="calcHeight transition-all duration-100 bg-dark-midnight text-white">
       <div className="flex flex-col gap-5 container m-auto px-4 py-10">
         <h1 className="text-2xl font-bold tracking-wide">Create a Link</h1>
         <hr />
-        <CreateLink/>
+        <CreateLink />
       </div>
     </section>
   )
