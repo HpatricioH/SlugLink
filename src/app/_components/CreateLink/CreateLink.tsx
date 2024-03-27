8985f85baa84c5f4121395ce4d4d40cc82c08128
HPHuerta <hhernanp@gmail.com>
2024-03-27 00:00:41 -0400

    Add form validation and error handling to CreateLink component

--- src/app/_components/CreateLink/CreateLink.tsx
+++ src/app/_components/CreateLink/CreateLink.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import { type FormEvent } from 'react'
 import { api } from '~/trpc/react'
@@ -8,11 +8,14 @@ import Button from '~/utils/Button'
 import { successToastHandler, errorToastHandler } from "~/utils/toastHandler";
 
 export default function CreateLink() {
+  const [formError, setFormError] = useState(false)
+  const [slugError, setSlugError] = useState(false)
   const ref = useRef<HTMLFormElement>(null)
   const router = useRouter()
-  const inputClass = "rounded-2xl bg-white/10 w-full mt-1 block px-3 py-2 border border-white/10 text-sm shadow-sm placeholder-white/50 focus:outline-none focus:ring-1 focus:ring-white/10 disabled:shadow-none"
+  const inputClass = 'rounded-2xl bg-white/10 w-full mt-1 block px-3 py-2 border border-white/10 text-sm shadow-sm placeholder-white/50 focus:outline-none focus:ring-1 focus:ring-white/10 disabled:shadow-none'
 
   const createLink = api.link.create.useMutation()
+  const errorUrl = createLink.error?.data?.zodError?.fieldErrors?.url?.[0]
 
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
@@ -23,20 +26,31 @@ export default function CreateLink() {
       description = ''
     } = Object.fromEntries(formData) as Record<string, string>
 
-    createLink.mutate({
-      url,
-      slug,
-      description
-    }, {
-      onSuccess: () => {
-        successToastHandler({ message: 'Link created successfully!'})
-        router.push('/dashboard')
-        router.refresh()
-      },
-      onError: (error) => {
-        errorToastHandler({ message: 'Slug already exists! Try another customize link.' })
-      }
-    })
+    if (!url) return setFormError(true)
+    if (!slug) return setSlugError(true)
+
+    if (url && slug) {
+      createLink.mutate({
+        url,
+        slug,
+        description
+      }, {
+        onSuccess: () => {
+          successToastHandler({ message: 'Link created successfully!' })
+          router.push('/dashboard')
+          router.refresh()
+        },
+        onError: () => {
+          if (errorUrl) {
+            setFormError(true)
+            errorToastHandler({ message: errorUrl })
+          } else {
+            errorToastHandler({ message: 'Slug already exists! Try another customize link.' })
+          }
+        }
+      })
+    }
+
   }
 
   return (
@@ -52,13 +66,15 @@ export default function CreateLink() {
         type="text"
         name='url'
         placeholder="https://example.com"
-        className={inputClass} />
+        className={`${inputClass} ${formError ? 'border border-red-700' : ''}`}
+        onFocus={() => setFormError(false)} />
       <label htmlFor="">Customize your link:</label>
       <input
         type="text"
         name='slug'
         placeholder="https://sluglink.com/your-link"
-        className={inputClass} />
+        className={`${inputClass} ${slugError ? 'border border-red-700' : ''}`}
+        onFocus={() => setSlugError(false)} />
       <label htmlFor="">Description (Optional):</label>
       <textarea
         name="description"
