bfba963ac9aa2738a3e5bbf1437df70accb71391
HPHuerta <hhernanp@gmail.com>
2024-03-29 23:28:56 -0400

    Refactor Edit component to include useEffect hook

--- src/app/_components/Edit/Edit.tsx
+++ src/app/_components/Edit/Edit.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useRouter } from "next/navigation";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { type FormEvent } from 'react'
 import Button from "~/utils/Button";
 import { successToastHandler } from "~/utils/toastHandler";
@@ -16,11 +16,21 @@ export default function Edit(props: EditProps) {
   const ref = useRef<HTMLFormElement>(null)
   const router = useRouter()
 
+  const getLink = trpc.link.getLink.useQuery({ id: props.id });
+  const editLinkMutation = trpc.link.updateLink.useMutation();
+
   const inputClass = 'rounded-2xl bg-white/10 w-full block px-3 py-2 border text-sm shadow-sm placeholder-white/50 focus:outline-none focus:ring-1 focus:ring-white/10 disabled:shadow-none'
   const buttonClass = "border border-white rounded-3xl p-2 hover:border-white/80 hover:text-white/50";
 
-  const getLink = trpc.link.getLink.useQuery({ id: props.id });
-  const editLinkMutation = trpc.link.updateLink.useMutation();
+  useEffect(() => {
+    if (editLinkMutation.isSuccess) {
+      getLink.refetch().then(() => {
+        router.refresh()
+      }).catch((error) => {
+        throw new Error(error as string)
+      });
+    }
+  }, [editLinkMutation.isSuccess, getLink, router])
 
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -38,7 +48,6 @@ export default function Edit(props: EditProps) {
     }, {
       onSuccess: () => {
         successToastHandler({ message: 'Link updated successfully!' })
-        router.refresh()
         props.setEditModal(false)
       }
     })
@@ -49,39 +58,37 @@ export default function Edit(props: EditProps) {
   }
 
   return (
-    <>
-      <form
-        ref={ref}
-        className="flex flex-col gap-3 text-white"
-        onSubmit={async (e) => {
-          await onSubmit(e)
-        }}
-      >
-        <label className="pt-3">New long URL:</label>
-        <input
-          type="text"
-          name='url'
-          placeholder="https://example.com"
-          className={`${inputClass} border-white/10`}
-          defaultValue={getLink.data?.url}
-        />
-        <label className="pt-3">Description (Optional):</label>
-        <textarea
-          className={`${inputClass} resize-none border-white/10`}
-          name="description"
-          rows={3}
-          cols={50}
-          placeholder="e.g. URL for my blog post"
-          defaultValue={getLink.data?.description ?? ''}
-        />
-        <div className="flex justify-end gap-3 pt-4">
-          <Button className={buttonClass}
-            disabled={editLinkMutation.isLoading}
-          > Edit Link
-          </Button>
-          <Button type="button" className={buttonClass} onClick={handleCloseModal}>Cancel</Button>
-        </div>
-      </form>
-    </>
+    <form
+      ref={ref}
+      className="flex flex-col gap-3 text-white"
+      onSubmit={async (e) => {
+        await onSubmit(e)
+      }}
+    >
+      <label className="pt-3">New long URL:</label>
+      <input
+        type="text"
+        name='url'
+        placeholder="https://example.com"
+        className={`${inputClass} border-white/10`}
+        defaultValue={getLink.data?.url}
+      />
+      <label className="pt-3">Description (Optional):</label>
+      <textarea
+        className={`${inputClass} resize-none border-white/10`}
+        name="description"
+        rows={3}
+        cols={50}
+        placeholder="e.g. URL for my blog post"
+        defaultValue={getLink.data?.description ?? ''}
+      />
+      <div className="flex justify-end gap-3 pt-4">
+        <Button className={buttonClass}
+          disabled={editLinkMutation.isLoading}
+        > Edit Link
+        </Button>
+        <Button type="button" className={buttonClass} onClick={handleCloseModal}>Cancel</Button>
+      </div>
+    </form>
   )
 }
