44542bf0d8da71d912fafc7323b011c4aa5c4f26
HPHuerta <hhernanp@gmail.com>
2024-04-05 23:56:42 -0400

    Fix button styling and add ButtonError component

--- src/app/_components/Delete/Delete.tsx
+++ src/app/_components/Delete/Delete.tsx
@@ -4,22 +4,22 @@ import { useRouter } from 'next/navigation';
 import { trpc } from "~/utils/trpc";
 import Button from "~/utils/Button";
 import { successToastHandler } from '~/utils/toastHandler';
+import ButtonError from '~/utils/ButtonError';
 
 interface DeleteProps {
   setDeleteModal: (value: boolean) => void;
   id: number;
 }
 
-export default function Delete (props:DeleteProps) {
+export default function Delete(props: DeleteProps) {
   const router = useRouter()
-  const buttonClass = "border border-white rounded-3xl p-2 hover:border-white/80 hover:text-white/50";
 
   const deleteLinkMutation = trpc.link.deleteLink.useMutation();
 
   const handleDeleteLink = () => {
-    deleteLinkMutation.mutate({id: props.id}, {
+    deleteLinkMutation.mutate({ id: props.id }, {
       onSuccess: () => {
-        successToastHandler({ message: 'Link deleted successfully!'})
+        successToastHandler({ message: 'Link deleted successfully!' })
         props.setDeleteModal(false);
         router.refresh()
       }
@@ -34,8 +34,12 @@ export default function Delete (props:DeleteProps) {
     <div className="text-white/50 pt-4">
       <p>If the link is deleted, this action cannot be reversed.</p>
       <div className="flex justify-end gap-3 pt-7 text-white">
-        <Button className={buttonClass} onClick={handleDeleteLink}>Delete Link</Button>
-        <Button className={buttonClass} onClick={handleCloseModal}>Cancel</Button>
+        <ButtonError
+          onClick={handleDeleteLink}
+          className={`${deleteLinkMutation.isLoading ? 'btn-loading' : ''} `}>
+          Delete Link
+        </ButtonError>
+        <Button onClick={handleCloseModal}>Cancel</Button>
       </div>
     </div>
   )
