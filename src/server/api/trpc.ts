89d1be4a7274e9a970453089719e93706d10153f
HPHuerta <hhernanp@gmail.com>
2024-04-23 23:27:50 -0400

    clean comments and delete posts endpoint that are not needed

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -1,12 +1,3 @@
-/**
- * YOU PROBABLY DON'T NEED TO EDIT THIS FILE, UNLESS:
- * 1. You want to modify request context (see Part 1).
- * 2. You want to create a new middleware or type of procedure (see Part 3).
- *
- * TL;DR - This is where all the tRPC server stuff is created and plugged in. The pieces you will
- * need to use are documented accordingly near the end.
- */
-
 import { initTRPC, TRPCError } from "@trpc/server";
 import superjson from "superjson";
 import { ZodError } from "zod";
@@ -59,14 +50,9 @@ const t = initTRPC.context<typeof createTRPCContext>().create({
 
 /**
  * 3. ROUTER & PROCEDURE (THE IMPORTANT BIT)
- *
- * These are the pieces you use to build your tRPC API. You should import these a lot in the
- * "/src/server/api/routers" directory.
  */
 
 /**
- * This is how you create new routers and sub-routers in your tRPC API.
- *
  * @see https://trpc.io/docs/router
  */
 export const createTRPCRouter = t.router;
@@ -82,10 +68,6 @@ export const publicProcedure = t.procedure;
 
 /**
  * Protected (authenticated) procedure
- *
- * If you want a query or mutation to ONLY be accessible to logged in users, use this. It verifies
- * the session is valid and guarantees `ctx.session.user` is not null.
- *
  * @see https://trpc.io/docs/procedures
  */
 export const protectedProcedure = t.procedure.use(({ ctx, next }) => {
